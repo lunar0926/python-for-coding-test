b264aa2ca4100f96057d05620872284b8008a611
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2021-03-11 16:18:15 +0900

    Create factorial.py
    팩토리얼을 구현하면서 재귀함수의 예시를 살펴보기

---
+++ python/factorial.py
@@ -0,0 +1,16 @@
+# 팩토리얼 구현하기 n!
+# 반복적으로 구현
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+# 재귀적으로 구현
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n - 1)
+
+# print(factorial_iterative(5))
+print(factorial_recursive(5))
